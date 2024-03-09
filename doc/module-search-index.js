e9dfc08a341cd2fabdd518cdaf769fc2bcb4f686
Emilio S. Sánchez Jiménez <juanipiz@hotmail.com>
2024-03-09 18:20:54 +0100

    Add files via upload

---
+++ doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"ED04DAM2024"}];updateSearchResults();
---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"ED04DAM2024","l":"entidadFinanciera"}];updateSearchResults();
